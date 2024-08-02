18cc28e9fa0a782b1762cfcd21e364e601ce8b79
Zachary Fischer <zfischer@dutchessny.gov>
2024-08-02 12:05:58 -0400

    adding .gitignore

--- src/Better Steps Recorder/obj/Release/net8.0-windows/Better Steps Recorder.AssemblyInfo.cs
+++ src/Better Steps Recorder/obj/Release/net8.0-windows/Better Steps Recorder.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Better Steps Recorder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55dae2cc2d9b494be82f1dfbcf53464eada46c95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e072e9d6f072e84d52fcda551eb0fb324899c48")]
 [assembly: System.Reflection.AssemblyProductAttribute("Better Steps Recorder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Better Steps Recorder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
